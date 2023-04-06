1dd7ac5c17efb0020af78aa638118480c95ff51f
Roberson Ferreira Cardoso <120033226+robersonweb@users.noreply.github.com>
2023-04-06 00:09:44 -0300

    atualizei exmodelo 15

--- aula12/ex015modelo2/script.js
+++ aula12/ex015modelo2/script.js
@@ -1,6 +1,57 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('itxtano')
-    var res = document.querySelector('div#res')
+    var fano = document.getElementById('txtano')
+    // var res = document.querySelector('div#res')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verificar os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        // res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute ('src', 'foto-bebe-m-250.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute ('src', 'foto-jovem-m-250.png' )
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute ('src', 'foto-adulto-m-250.png')
+            } else {
+                //idoso
+                img.setAttribute ('src', 'foto-idoso-m-250.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //criança
+                img.setAttribute ('src', 'foto-bebe-f-250.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute ('src', 'foto-jovem-f-250.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute ('src', 'foto-adulto-f-250.png')
+            } else {
+                //idoso
+                img.setAttribute ('src', 'foto-idosa-f-250.png')
+            }
+ 
+
+         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+
+    }
+
 }
