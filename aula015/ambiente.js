4c6c4cd9e144237739cbad44203fb50f222aac55
Roberson Ferreira Cardoso <120033226+robersonweb@users.noreply.github.com>
2023-04-17 10:36:11 -0300

    criei ex ambiente

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,2 +1,4 @@
-let num = [3,5,8,4]
-    console.log(`Nosso vetor é o ${num}`)
+// let num = [3,5,8,4 ]
+//     console.log(`Nosso vetor é o ${num}`)
+let num = [1,8,9,6,15]
+console.log(`O vetor tem ${num.length} posições`)
---
+++ aula015/vetornatela.js
@@ -0,0 +1,15 @@
+//let valores = [4, 8, 3, 5]
+//console.log(valores) //mostra o vetor
+// valores.sort()
+// console.log(valores[0])
+// console.log(valores[1])
+// console.log(valores[2])
+// console.log(valores[3])  
+// for (let pos = 0; pos < valores.length; pos++) {
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+// }
+
+
+let valores = [4, 8, 3, 5]
+let pos = valores.indexOf(3)
+console.log(`O valor está na posição ${pos}`)
