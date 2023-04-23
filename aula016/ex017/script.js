cb22acfc8a3f7e0fdfa4131da8fe26af970bd3dc
Roberson Ferreira Cardoso <120033226+robersonweb@users.noreply.github.com>
2023-04-23 00:22:34 -0300

    atualizei

--- aula016/ex017/script.js
+++ aula016/ex017/script.js
@@ -52,7 +52,7 @@ function finalizar (){
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
-        res.innerHTML += `<p>O maior valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
         res.innerHTML += ` <p>Somando todos os valores, temos ${soma}.</p>`
 
 
